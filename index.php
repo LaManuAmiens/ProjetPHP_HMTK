af6197d7dc352b7cd86915ce1de45a3ddb7066c6
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-12-14 09:45:52 +0100

    toto

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
   <meta charset="utf-8" />
   <title>ProjetPHP_HMKT</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/stylet.css">
 </head>
 <body>
   <div class="container-fluid">
+<!-- ?php $fichier = 'source.xml';
+$xml = simplexml_load_file($fichier);
+foreach($xml as $page){
+	echo $content->content.'<br>';
+} ?> -->
+<div class='jumbotron'>
+<?php
+$xml=simplexml_load_file("source.xml") or die("Error: Cannot create object");
+echo $xml->page[1]->menu .  $xml->page[1]->title . $xml->page[1]->content;"<br>";
+?>
 
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
