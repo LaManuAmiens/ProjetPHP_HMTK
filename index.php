8e9d72d080ce25dc6c271c851a5adbbd8b2d7e87
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-12-14 09:29:04 +0100

    mickael

--- index.php
+++ index.php
@@ -11,21 +11,21 @@
         <div class="container-fluid">
             <div class="jumbotron">
                 <a class="nav-link" href="index.php?id=0.html">Acceuil</a>
-                 <a class="nav-link" href="index.php?id=1.html">Qui sommes-nous ?</a>
-                
-<?php 
-$fichier = 'source.xml';
-$xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
-echo $xml->page[0]->menu . $xml->page[0]->title . $xml->page[0]->content ;
+                 <a class="nav-link" href="index.php?id=1.html">Qui sommes-nous ?</a>               
+                <?php 
+                $fichier = 'source.xml';
+                $xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
 
-
-$fichier = 'source.xml';
-$xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
-echo $xml->page[1]->menu . $xml->page[1]->title . $xml->page[1]->content ;
-
-
-?>
-            </div>
+                 if($_GET['id'] == '0.html'){
+                        echo $xml->page[0]->menu .
+                            $xml->page[0]->title .
+                            $xml->page[0]->content ;
+                 }
+                else{
+                  echo 'error';
+                }
+                ?>
+             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
