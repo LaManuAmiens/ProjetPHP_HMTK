a833da52b8c2c038c06d2d8a72abba8f2aa34500
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-12-14 19:37:53 +0100

    mickael

--- index.php
+++ index.php
@@ -21,21 +21,31 @@
             <?php
                 }   
                 ?> 
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/style.css">      
     </head>
     <body>
-        <div class="topnav" id="myTopnav">
-            <a class="nav-link" href="index.php?id=0.html"><?= $xml->page[0]->menu ;?></a>
-            <a class="nav-link" href="index.php?id=1.html"><?= $xml->page[1]->menu ;?></a>
-            <a class="nav-link" href="index.php?id=2.html"><?= $xml->page[2]->menu ;?></a>
-            <a class="nav-link" href="index.php?id=3.html"><?= $xml->page[3]->menu ;?></a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
+       <div class="container-fluid">
+
+        
+        <div class="button_container" id="toggle">
+            <span class="top"></span>
+            <span class="middle"></span>
+            <span class="bottom"></span>
         </div>
-        <div class="container-fluid">
+        
+        <div class="overlay" id="overlay">
+            <nav class="overlay-menu">
+                <ul>
+                    <li><a href="index.php?id=0.html"><?= $xml->page[0]->menu ;?></a></li>
+                    <li><a href="index.php?id=1.html"><?= $xml->page[1]->menu ;?></a></li>
+                    <li><a href="index.php?id=2.html"><?= $xml->page[2]->menu ;?></a></li>
+                    <li><a href="index.php?id=3.html"><?= $xml->page[3]->menu ;?></a></li>
+                </ul>
+            </nav>
+        </div>
+      
             <div class="jumbotron">
                 <?php                                               
                  if($_GET['id'] == '1.html'){?>                                        
@@ -52,20 +62,11 @@
             <?php
                 }   
                 ?>
-            </div>
+               </div>
         </div>
-        <script>
-            function myFunction() {
-                var x = document.getElementById("myTopnav");
-                if (x.className === "topnav") {
-                    x.className += " responsive";
-                } else {
-                    x.className = "topnav";
-                }
-            }
-        </script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js'></script>
+        <script  src="index.js"></script>
     </body>
 </html>
