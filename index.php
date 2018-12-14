c374c0fcdb208b3a25e66456a968802adcc63c57
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-12-14 11:36:57 +0100

    mickael

--- index.php
+++ index.php
@@ -13,31 +13,35 @@
                 <a class="nav-link" href="index.php?id=0.html">Acceuil</a>
                  <a class="nav-link" href="index.php?id=1.html">Qui sommes-nous ?</a>    
                  <a class="nav-link" href="index.php?id=2.html">Nos clients témoignent</a>
-                 <a class="nav-link" href="index.php?id=3.html">Contact</a>
+                 <a class="nav-link" href="index.php?id=3.html">Contacts</a>
                 <?php 
                 $fichier = 'source.xml';
                 $xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
-                         $xml->page[0]->menu .
-                            $xml->page[0]->title .
-                            $xml->page[0]->content ;
+            
                          
-                 if($_GET['id'] == '1.html'){
+                 if($_GET['id'] == '1.html'){?>
                      
-                        echo $xml->page[1]->menu .
-                            $xml->page[1]->title .
-                            $xml->page[1]->content ;
-                        
+                        <h1><?= $xml->page[1]->menu ;?></h1>
+                        <h1><?= $xml->page[1]->title ;?></h1>
+                        <p><?= $xml->page[1]->content ;?></p>
+                <?php      
                  }else if ($_GET['id'] == '2.html'){
                 
                      echo $xml->page[2]->menu .
                             $xml->page[2]->title .
                             $xml->page[2]->content ;
                
-                }else{
-                    
-                      echo $xml->page[3]->menu .
+                }else if ($_GET['id'] == '3.html'){
+                
+                     echo $xml->page[3]->menu .
                             $xml->page[3]->title .
                             $xml->page[3]->content ;
+               
+                }else{
+                    
+                      echo $xml->page[0]->menu .
+                            $xml->page[0]->title .
+                            $xml->page[0]->content ;
                 }   
                 ?>
              </div>
