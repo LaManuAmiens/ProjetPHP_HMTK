e9034ce3186d30f9b29fa701e9b55ea9c4019d8a
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-12-14 09:06:32 +0100

    mickael

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
+
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>ProjetPHP_HMKT</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
-  <div class="container-fluid">
+    <head>
+        <meta charset="utf-8" />
+        <title>ProjetPHP_HMKT</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <a class="nav-link" href="index.php?id=0.html">Acceuil</a>
+                 <a class="nav-link" href="index.php?id=1.html">Qui sommes-nous ?</a>
+                
+<?php 
+$fichier = 'source.xml';
+$xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
+echo $xml->page[0]->menu . $xml->page[0]->title . $xml->page[0]->content ;
 
-  </div>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-</body>
+
+$fichier = 'source.xml';
+$xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
+echo $xml->page[1]->menu . $xml->page[1]->title . $xml->page[1]->content ;
+
+
+?>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    </body>
 </html>
