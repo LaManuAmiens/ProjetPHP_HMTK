15fb8f2f1d45f851a066d428401f743f4fa9b959
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-12-14 12:00:10 +0100

    mickael

--- index.php
+++ index.php
@@ -9,27 +9,24 @@
     </head>
     <body>
         <div class="topnav" id="myTopnav">
-    <a href="index.php?id=0.html">Acceuil</a>
-    <a href="index.php?id=1.html">Qui sommes-nous ?</a>
-    <a href="index.php?id=2.html">Nos clients témoignent</a>
-    <a class="nav-link" href="index.php?id=3.html">Contacts</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-      <i class="fa fa-bars"></i>
-      </a>
-    </div>
+            <a href="index.php?id=0.html">Acceuil</a>
+            <a href="index.php?id=1.html">Qui sommes-nous ?</a>
+            <a href="index.php?id=2.html">Nos clients témoignent</a>
+            <a class="nav-link" href="index.php?id=3.html">Contacts</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
         <div class="container-fluid">
             <div class="jumbotron">
-            
                 <?php 
                 $fichier = 'source.xml';
                 $xml = simplexml_load_file($fichier) or die("Error: Cannot create object");
-            
-                         
+                                     
                  if($_GET['id'] == '1.html'){?>
-                     
-              
-                        <title><?= $xml->page[1]->title ;?></title>
-                        <p><?= $xml->page[1]->content ;?></p>
+                                
+                <title><?= $xml->page[1]->title ;?></title>
+                <p><?= $xml->page[1]->content ;?></p>
                 <?php      
                  }else if ($_GET['id'] == '2.html'){
                 
@@ -45,23 +42,22 @@
                
                 }else{
                     
-                      echo 
-                            $xml->page[0]->title .
+                      echo $xml->page[0]->title .
                             $xml->page[0]->content ;
                 }   
                 ?>
-             </div>
+            </div>
         </div>
         <script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-      x.className += " responsive";
-  } else {
-      x.className = "topnav";
-  }
-}
-</script>
+            function myFunction() {
+                var x = document.getElementById("myTopnav");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+            }
+        </script>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
